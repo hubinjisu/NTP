94d2999809bbfc1753d6da4bd197037a2fb83dc6
hubinjisu <hubinjisu@hotmail.com>
2016-04-02 15:48:10 +0800

    with junit test

--- app/src/main/java/com/android/hubin/ntp/MainActivity.java
+++ app/src/main/java/com/android/hubin/ntp/MainActivity.java
@@ -1,21 +1,16 @@
 package com.android.hubin.ntp;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.android.hubin.ntp.manager.NtpManager;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends Activity
 {
     private EditText ntpServerET;
     private TextView ntpTimeTV;
@@ -26,7 +21,6 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         ntpServerET = (EditText) findViewById(R.id.ntp_server);
         ntpTimeTV = (TextView) findViewById(R.id.result);
         startBtn = (Button) findViewById(R.id.start_ntp);
@@ -41,42 +35,6 @@ public class MainActivity extends AppCompatActivity
                 }
             }
         });
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View view)
-            {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings)
-        {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
