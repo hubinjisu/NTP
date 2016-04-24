c2444f96fb4188ddb5328a35171395a6ab5b6cc8
hubinjisu <hubinjisu@hotmail.com>
2016-04-24 16:39:27 +0800

    with junit test

--- app/src/main/java/com/android/hubin/ntp/MainActivity.java
+++ app/src/main/java/com/android/hubin/ntp/MainActivity.java
@@ -8,8 +8,6 @@ import android.content.IntentFilter;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
