a0f57de81cf7e34e1a02c69662282b2789c0e7ca
hubinjisu <hubinjisu@hotmail.com>
2016-04-24 16:39:09 +0800

    with junit test

--- app/src/main/java/com/android/hubin/ntp/MainActivity.java
+++ app/src/main/java/com/android/hubin/ntp/MainActivity.java
@@ -1,14 +1,11 @@
 package com.android.hubin.ntp;
 
+import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.Menu;
@@ -23,7 +20,7 @@ import com.android.hubin.ntp.manager.NtpManager;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends Activity
 {
     private static final String TAG = "MainActivity";
     private EditText ntpServerET;
@@ -37,7 +34,6 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         ntpServerET = (EditText) findViewById(R.id.ntp_server);
         ntpTimeTV = (TextView) findViewById(R.id.result);
         startBtn = (Button) findViewById(R.id.start_ntp);
@@ -51,18 +47,6 @@ public class MainActivity extends AppCompatActivity
                 {
                     ntpManager.startNtpService(ntpServerET.getText().toString(), 123, 123, 27, 30);
                 }
-            }
-        });
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View view)
-            {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
             }
         });
 
@@ -97,34 +81,10 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
     protected void onDestroy()
     {
         unregisterReceiver(receiver);
         super.onDestroy();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings)
-        {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
